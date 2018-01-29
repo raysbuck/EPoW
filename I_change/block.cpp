39309f6225b6aaa9f41bcbd99c570682aeea0313
raysbuck <b04902049@csie.ntu.edu.tw>
2018-01-29 12:26:44 +0800

    0130v2

---
+++ I_change/block.cpp
@@ -0,0 +1,45 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2016 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "primitives/block.h"
+
+#include "hash.h"
+#include "tinyformat.h"
+#include "utilstrencodings.h"
+#include "crypto/common.h"
+
+//b04902091
+uint256 CBlockHeader::GetHash() const
+{
+        CHashWriter ss(SER_GETHASH, PROTOCOL_VERSION);
+        
+        ss<<nVersion<<hashPrevBlock<<hashMerkleRoot<<nTime<<nBits<<nNonce;
+        
+        return ss.GetHash();
+}
+uint256 CBlockHeader::GetHash2() const
+{
+        CHashWriter ss(SER_GETHASH, PROTOCOL_VERSION);
+        
+        ss<<nVersion<<hashPrevBlock<<hashMerkleRoot2<<nTime<<nBits<<nNonce2;
+        
+        return ss.GetHash();
+}
+
+std::string CBlock::ToString() const
+{
+    std::stringstream s;
+    s << strprintf("CBlock(hash=%s, ver=0x%08x, hashPrevBlock=%s, hashMerkleRoot=%s, nTime=%u, nBits=%08x, nNonce=%u, vtx=%u)\n",
+        GetHash().ToString(),
+        nVersion,
+        hashPrevBlock.ToString(),
+        hashMerkleRoot.ToString(),
+        nTime, nBits, nNonce,
+        vtx.size());
+    for (const auto& tx : vtx) {
+        s << "  " << tx->ToString() << "\n";
+    }
+    return s.str();
+}
